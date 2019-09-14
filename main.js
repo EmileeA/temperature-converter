c4fc6924aeff3789162670b0ce59be9ea08ca8ba
Emilee Mitchell <emcakes@gmail.com>
2019-09-13 21:45:55 -0500

    I tried to get that event listener to work

--- main.js
+++ main.js
@@ -1,14 +1,31 @@
 console.log("hi from js");
 
-const printToDom = (divId, textToPrint) => {
+const printToDom = (divId, textToPrint) =>
+ {
   const selectedDiv = document.getElementById(divId);
-  selectedDiv.innerHTML = textToPrint;
-};
+ selectedDiv.innerHTML = textToPrint;
+ };
+
+  const celsiusButton = document.getElementById("gridRadios1").checked;
+  const fahrenheitButton = document.getElementById("gridRadios2").checked;
+  const temp = document.getElementById("number").value;
+ 
 
 //Boiler Plate
-const toCelsius = () => {};
+const toCelsius = () => {
+if (celsiusButton === 'true') {
+    const newTemp = Math.floor(temp * 9 / 5 + 32);
+}
+printToDOM('output', convertedCTemp);
+};
 
-const toFahrenheit = () => {};
+
+const toFahrenheit =  () => {
+    if (fahrenheitButton === 'true') {
+        const newTempF = Math.floor((temp - 32) * 5 / 9);
+    }
+    printToDOM('output', convertedFTemp);
+};
 
 // Get a reference to the button element in the DOM
 const button = document.getElementById("converter");
@@ -17,6 +34,11 @@ const button = document.getElementById("converter");
 // happen based on which radio button is selected.
 const determineConverter = e => {
   console.log("event", e);
+  const buttonID = e.target.id;
+  if (buttonID === 'convertButton'&& fahrenheitButton === 'true') {
+    toFahrenheit();
+  } else if (buttonID === 'convertButton' && celsiusButton === 'true') {
+      toCelsius();
 };
 
 // Assign a function to be executed when the button is clicked
